349d1d7fed1ed255000ff6e7176f51a308c0293f
Ash0218 <ajykim218@gmail.com>
2022-07-03 21:25:22 -0400

    Deleted String_Frequencies class

--- src/test/java/Java_Collections/String_Frequencies_Find.java
+++ src/test/java/Java_Collections/String_Frequencies_Find.java
@@ -1,4 +1,22 @@
 package Java_Collections;
 
 public class String_Frequencies_Find {
+    public static void main(String[] args) {
+        String str = "AAABBCCCCDEEE";
+        String expectedResult = "";
+
+        for (int j = 0; j < str.length(); j++){
+            int count = 0;
+
+            for (int i = 0; i < str.length(); i++){
+                if (str.charAt(i) == str.charAt(j)){
+                    count ++;
+                }
+            }
+
+            expectedResult += "" + str.charAt(j) + count;
+            str = str.replace("" + str.charAt(j), "");
+        }
+        System.out.println(expectedResult);
+    }
 }
